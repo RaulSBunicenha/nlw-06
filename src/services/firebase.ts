56b1d1533a640b20ecfdf03997f16c582dab70a6
Raul B <raulbunicenha@gmail.com>
2021-06-21 16:05:00 -0300

    Config Credentials

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,13 +4,13 @@ import 'firebase/auth'
 import 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDsWMAI0_SzeSMBKOEa6VkxaPMq3z-GH28",
-  authDomain: "nlw-letmeas.firebaseapp.com",
-  databaseURL: "https://nlw-letmeas-default-rtdb.firebaseio.com",
-  projectId: "nlw-letmeas",
-  storageBucket: "nlw-letmeas.appspot.com",
-  messagingSenderId: "9884216430",
-  appId: "1:9884216430:web:b447f3133451e5f112ca7f"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 }
 
 firebase.initializeApp(firebaseConfig)
